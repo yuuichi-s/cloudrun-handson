aac409c58c35f0287d88376e0b1efc5b943cda3f
yuuichi-s <lov.music.jaz@gmail.com>
2024-06-15 02:13:57 +0000

    feat: hands-on step2

--- app/frontend/main.go
+++ app/frontend/main.go
@@ -19,7 +19,7 @@ type Response struct {
 
 func main() {
 	http.HandleFunc("/frontend", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello first hands-on!!\n")
+		fmt.Fprintf(w, "Hello first hands-on!! hello user!\n")
 	})
 
 	http.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
